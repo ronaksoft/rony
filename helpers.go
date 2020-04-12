c85f1391416b1e6112429a530b3e12ab40bb4f9d
Ehsan N. Moosa <ehsan.code@nested.me>
2020-04-12 20:08:07 +0430

    helper function ConstructorOf implemented

---
+++ helpers.go
@@ -0,0 +1,24 @@
+package rony
+
+import (
+	"git.ronaksoftware.com/ronak/rony/internal/tools"
+	"hash/crc64"
+	"reflect"
+)
+
+/*
+   Creation Time: 2020 - Apr - 12
+   Created by:  (ehsan)
+   Maintainers:
+      1.  Ehsan N. Moosa (E2)
+   Auditor: Ehsan N. Moosa (E2)
+   Copyright Ronak Software Group 2020
+*/
+
+var (
+	crc64Table = crc64.MakeTable(crc64.ISO)
+)
+
+func ConstructorOf(x interface{}) int64 {
+	return int64(crc64.Checksum(tools.StrToByte(reflect.ValueOf(x).Type().Name()), crc64Table))
+}
---
+++ helpers_test.go
@@ -0,0 +1,16 @@
+package rony
+
+import "testing"
+
+/*
+   Creation Time: 2020 - Apr - 12
+   Created by:  (ehsan)
+   Maintainers:
+      1.  Ehsan N. Moosa (E2)
+   Auditor: Ehsan N. Moosa (E2)
+   Copyright Ronak Software Group 2020
+*/
+
+func TestConstructorOf(t *testing.T) {
+	ConstructorOf(MessageEnvelope{})
+}
