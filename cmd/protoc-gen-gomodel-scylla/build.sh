f8a240e26dcb3e668f1d22acfaa18ce0fc41591f
Ehsan N. Moosa <ehsan.code@nested.me>
2020-07-31 13:18:53 +0430

    some changes

--- cmd/protoc-gen-gomodel-scylla/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-protoc  -I=./testdata  --gorony_out=./testdata ./testdata/*.proto
--- cmd/rony/main.go
+++ cmd/rony/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"git.ronaksoftware.com/ronak/rony/cmd/rony/plugins/pools"
+	"git.ronaksoftware.com/ronak/rony/cmd/rony/plugins/rpc"
 	"git.ronaksoftware.com/ronak/rony/tools"
-	"github.com/gogo/protobuf/proto"
-	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
 	"github.com/gogo/protobuf/vanity"
 	"github.com/gogo/protobuf/vanity/command"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -46,46 +44,27 @@ var RootCmd = &cobra.Command{
 				if info.IsDir() {
 					return nil
 				}
+				protoParseCmd := getProtocCmd([]string{fmt.Sprintf(
+					"-I=%s", dir),
+					"--include_imports",
+					"--include_source_info",
+					"--descriptor_set_out=/dev/stdout",
+					path,
+				})
+				protoCompileCmd := getProtocCmd([]string{"--descriptor_set_in=/dev/stdin"})
 
-				protocArgs = append(protocArgs[:0], " --descriptor_set_out", "/dev/stdout")
-				err = runProtoc(os.Stdout, os.Stdin, protocArgs, path)
-				if err != nil {
-					return nil
-				}
+				protoParseCmd.Stdout = os.Stdin
+				protoCompileCmd.Stdin = os.Stdout
 
-				// go func() {
-				g := generator.New()
+				err = protoParseCmd.Start()
+				panicOnErr(err, "ProtoParser Start")
+				err = protoCompileCmd.Start()
+				panicOnErr(err, "ProtoCompiler Start")
 
-				fmt.Println("Lets read descriptors from stdin")
-				data, err := ioutil.ReadAll(os.Stdin)
-				if err != nil {
-					g.Error(err, "reading input")
-				}
+				go generate()
 
-				if err := proto.Unmarshal(data, g.Request); err != nil {
-					g.Error(err, "parsing input proto")
-				}
-
-				if len(g.Request.FileToGenerate) == 0 {
-					g.Fail("no files to generate")
-				}
-
-				files := g.Request.GetProtoFile()
-				files = vanity.FilterFiles(files, vanity.NotGoogleProtobufDescriptorProto)
-
-				vanity.ForEachFile(files, vanity.TurnOnMarshalerAll)
-				vanity.ForEachFile(files, vanity.TurnOnSizerAll)
-				vanity.ForEachFile(files, vanity.TurnOnUnmarshalerAll)
-
-				fmt.Println("Lets write generated code to the stdout")
-				command.Write(command.GeneratePlugin(g.Request, &pools.Generator{}, ".pools.go"))
-				// }()
-
-				protocArgs = append(protocArgs[:0], " --descriptor_set_in", "/dev/stdin")
-				err = runProtoc(os.Stdout, os.Stdin, protocArgs, path)
-				if err != nil {
-					fmt.Println("ERR! On Write", err.Error())
-				}
+				_ = protoParseCmd.Wait()
+				_ = protoCompileCmd.Wait()
 
 				return nil
 			})
@@ -94,18 +73,85 @@ var RootCmd = &cobra.Command{
 	},
 }
 
-func runProtoc(stdin, stdout *os.File, args []string, path string) error {
+func panicOnErr(err error, txt string) {
+	if err != nil {
+		panic(errors.Wrap(err, txt))
+	}
+}
+
+func getProtocCmd(args []string) *exec.Cmd {
 	var protocArgs []string
 	protocArgs = append(protocArgs, args...)
-	protocArgs = append(protocArgs, path)
 	protocCmd := exec.Command("protoc", protocArgs...)
-	fmt.Println("Check! ", path, protocCmd.String())
+	fmt.Println("Check! ", protocCmd.String())
 	protocCmd.Env = os.Environ()
-	protocCmd.Stdout = stdout
-	protocCmd.Stdin = stdin
-	err := protocCmd.Run()
-	if err != nil {
-		return err
-	}
-	return nil
+	protocCmd.Stdout = nil
+	return protocCmd
 }
+
+func generate() {
+	req := command.Read()
+	files := req.GetProtoFile()
+	files = vanity.FilterFiles(files, vanity.NotGoogleProtobufDescriptorProto)
+
+	vanity.ForEachFile(files, vanity.TurnOnMarshalerAll)
+	vanity.ForEachFile(files, vanity.TurnOnSizerAll)
+	vanity.ForEachFile(files, vanity.TurnOnUnmarshalerAll)
+
+	command.Write(command.GeneratePlugin(req, &rpc.Generator{}, ".rony.go"))
+}
+// func generate(input io.Reader, output io.Writer, fileSuffix string) error {
+// 	g := generator.New()
+// 	data, err := ioutil.ReadAll(input)
+// 	if err != nil {
+// 		g.Error(err, "reading input")
+// 	}
+//
+// 	if err := proto.Unmarshal(data, g.Request); err != nil {
+// 		g.Error(err, "parsing input proto")
+// 	}
+//
+// 	if len(g.Request.FileToGenerate) == 0 {
+// 		g.Fail("no files to generate")
+// 	}
+//
+// 	g.WrapTypes()
+// 	g.SetPackageNames()
+// 	g.BuildTypeNameMap()
+// 	g.GeneratePlugin(&pools.Generator{})
+//
+// 	for i := 0; i < len(g.Response.File); i++ {
+// 		g.Response.File[i].Name = proto.String(
+// 			strings.Replace(*g.Response.File[i].Name, ".pb.go", fileSuffix, -1),
+// 		)
+// 	}
+//
+// 	if err := goFormat(g.Response); err != nil {
+// 		g.Error(err)
+// 	}
+//
+// 	// Send back the results.
+// 	data, err = proto.Marshal(g.Response)
+// 	if err != nil {
+// 		g.Error(err, "failed to marshal output proto")
+// 	}
+//
+// 	_, err = output.Write(data)
+// 	if err != nil {
+// 		g.Error(err, "failed to write output proto")
+// 	}
+//
+// 	return err
+// }
+
+// func goFormat(resp *plugin.CodeGeneratorResponse) error {
+// 	for i := 0; i < len(resp.File); i++ {
+// 		formatted, err := format.Source([]byte(resp.File[i].GetContent()))
+// 		if err != nil {
+// 			return fmt.Errorf("go format error: %v", err)
+// 		}
+// 		fmts := string(formatted)
+// 		resp.File[i].Content = &fmts
+// 	}
+// 	return nil
+// }
