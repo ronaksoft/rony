72a2982a3ee9825c0ad2436aef27904bc04419d6
Ehsan N. Moosa <ehsan.code@nested.me>
2021-01-11 12:05:26 +0330

    readme updated

--- internal/testEnv/test_server.go
+++ internal/testEnv/test_server.go
@@ -69,7 +69,8 @@ func InitEdgeServerWithWebsocket(serverID string, listenPort int, concurrency in
 			ListenAddress: fmt.Sprintf(":%d", listenPort),
 			MaxIdleTime:   time.Second,
 			ExternalAddrs: []string{fmt.Sprintf("127.0.0.1:%d", listenPort)},
-		}),
+		},
+		),
 	)
 	edgeServer := edge.NewServer(serverID, opts...)
 
