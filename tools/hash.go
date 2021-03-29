387a7429e7e1e31577f06d817a7aebde29b9a845
Ehsan N. Moosa <ehsan.code@nested.me>
2021-03-29 17:26:14 +0430

    hash functions improved

--- tools/hash.go
+++ tools/hash.go
@@ -16,15 +16,6 @@ import (
    Copyright Ronak Software Group 2020
 */
 
-// Sha256 returns a 32bytes array which is sha256(in)
-func Sha256(in []byte) ([]byte, error) {
-	h := sha256.New()
-	if _, err := h.Write(in); err != nil {
-		return nil, err
-	}
-	return h.Sum(nil), nil
-}
-
 var poolSha512 = sync.Pool{
 	New: func() interface{} {
 		return sha512.New()
@@ -53,10 +44,32 @@ func MustSha512(in, out []byte) {
 	return
 }
 
-func MustSha256(in []byte) []byte {
-	h, err := Sha256(in)
+var poolSha256 = sync.Pool{
+	New: func() interface{} {
+		return sha256.New()
+	},
+}
+
+// Sha256 returns a 64bytes array which is sha256(in)
+func Sha256(in, out []byte) error {
+	h := poolSha256.Get().(hash.Hash)
+	if _, err := h.Write(in); err != nil {
+		h.Reset()
+		poolSha256.Put(h)
+		return err
+	}
+	h.Sum(out)
+	h.Reset()
+	poolSha256.Put(h)
+	return nil
+}
+
+func MustSha256(in, out []byte) {
+	err := Sha256(in, out)
 	if err != nil {
 		panic(err)
 	}
-	return h
+	return
 }
+
+
