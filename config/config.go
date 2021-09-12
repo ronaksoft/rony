44e75d5b141b7e89ba30ff1ef9ea58ade9ba79c0
Ehsan <ehsan@nested.me>
2021-09-12 18:19:11 +0300

    refactor config package

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -16,45 +17,72 @@ import (
    Copyright Ronak Software Group 2020
 */
 
+type Config struct {
+	v *viper.Viper
+}
+
 var (
-	_Viper *viper.Viper
+	once         = &sync.Once{}
+	globalConfig *Config
 )
 
 func Init() {
-	_Viper = viper.NewWithOptions(
-		viper.EnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_")),
-	)
+	once.Do(func() {
+		globalConfig = New()
+	})
 }
 
-func ReadFile(configFileName string, configSearchPaths ...string) error {
+func New() *Config {
+	return &Config{
+		v: viper.NewWithOptions(
+			viper.EnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_")),
+		),
+	}
+}
+
+func (c *Config) ReadFile(configFileName string, configSearchPaths ...string) error {
 	for _, p := range configSearchPaths {
-		_Viper.AddConfigPath(p)
+		c.v.AddConfigPath(p)
 	}
 	if configFileName == "" {
 		configFileName = "config"
 	}
 
-	_Viper.SetConfigName(configFileName)
+	c.v.SetConfigName(configFileName)
 
-	return _Viper.ReadInConfig()
+	return c.v.ReadInConfig()
+}
+
+func ReadFile(configFileName string, configSearchPaths ...string) error {
+	return globalConfig.ReadFile(configFileName, configSearchPaths...)
+}
+
+func (c *Config) AllSettings() map[string]interface{} {
+	return c.v.AllSettings()
 }
 
 func AllSettings() map[string]interface{} {
-	return _Viper.AllSettings()
+	return globalConfig.AllSettings()
 }
 
 // SetEnvPrefix defines a prefix that ENVIRONMENT variables will use.
 // // E.g. if your prefix is "spf", the env registry will look for env
 // // variables that start with "SPF_".
+func (c *Config) SetEnvPrefix(prefix string) {
+	c.v.SetEnvPrefix(prefix)
+	c.v.AutomaticEnv()
+}
 func SetEnvPrefix(prefix string) {
-	_Viper.SetEnvPrefix(prefix)
-	_Viper.AutomaticEnv()
+	globalConfig.SetEnvPrefix(prefix)
 }
 
 // BindCmdFlags binds a full flag set to the configuration, using each flag's long
 // name as the config key.
+func (c *Config) BindCmdFlags(cmd *cobra.Command) error {
+	return c.v.BindPFlags(cmd.Flags())
+}
 func BindCmdFlags(cmd *cobra.Command) error {
-	return _Viper.BindPFlags(cmd.Flags())
+	return globalConfig.BindCmdFlags(cmd)
 }
 
 // SetCmdFlags sets the flags defined by FlagOption to the command 'cmd'.
@@ -75,48 +103,79 @@ func SetCmdPersistentFlags(cmd *cobra.Command, opts ...FlagOption) {
 // Set is case-insensitive for a key.
 // Will be used instead of values obtained via
 // flags, config file, ENV, default, or key/value store.
+func (c *Config) Set(key string, val interface{}) {
+	c.v.Set(key, val)
+}
+
 func Set(key string, val interface{}) {
-	_Viper.Set(key, val)
+	globalConfig.Set(key, val)
 }
 
+func (c *Config) GetBool(key string) bool {
+	return c.v.GetBool(key)
+}
 func GetBool(key string) bool {
-	return _Viper.GetBool(key)
+	return globalConfig.GetBool(key)
 }
 
+func (c *Config) GetString(key string) string {
+	return c.v.GetString(key)
+}
 func GetString(key string) string {
-	return _Viper.GetString(key)
+	return globalConfig.GetString(key)
 }
 
+func (c *Config) GetInt64(key string) int64 {
+	return c.v.GetInt64(key)
+}
 func GetInt64(key string) int64 {
-	return _Viper.GetInt64(key)
+	return globalConfig.GetInt64(key)
 }
 
+func (c *Config) GetUint64(key string) uint64 {
+	return c.v.GetUint64(key)
+}
 func GetUint64(key string) uint64 {
-	return _Viper.GetUint64(key)
+	return globalConfig.GetUint64(key)
 }
 
+func (c *Config) GetInt32(key string) int32 {
+	return c.v.GetInt32(key)
+}
 func GetInt32(key string) int32 {
-	return _Viper.GetInt32(key)
+	return globalConfig.GetInt32(key)
 }
 
+func (c *Config) GetUint32(key string) uint32 {
+	return c.v.GetUint32(key)
+}
 func GetUint32(key string) uint32 {
-	return _Viper.GetUint32(key)
+	return globalConfig.GetUint32(key)
 }
 
+func (c *Config) GetInt(key string) int {
+	return c.v.GetInt(key)
+}
 func GetInt(key string) int {
-	return _Viper.GetInt(key)
+	return globalConfig.GetInt(key)
 }
 
+func (c *Config) GetUint(key string) uint {
+	return c.v.GetUint(key)
+}
 func GetUint(key string) uint {
-	return _Viper.GetUint(key)
+	return globalConfig.GetUint(key)
 }
 
+func (c *Config) GetIntSlice(key string) []int {
+	return c.v.GetIntSlice(key)
+}
 func GetIntSlice(key string) []int {
-	return _Viper.GetIntSlice(key)
+	return globalConfig.GetIntSlice(key)
 }
 
-func GetInt32Slice(key string) []int32 {
-	ints := _Viper.GetIntSlice(key)
+func (c *Config) GetInt32Slice(key string) []int32 {
+	ints := c.v.GetIntSlice(key)
 	int32s := make([]int32, 0, len(ints))
 	for _, i := range ints {
 		int32s = append(int32s, int32(i))
@@ -124,9 +183,12 @@ func GetInt32Slice(key string) []int32 {
 
 	return int32s
 }
+func GetInt32Slice(key string) []int32 {
+	return globalConfig.GetInt32Slice(key)
+}
 
-func GetInt64Slice(key string) []int64 {
-	ints := _Viper.GetIntSlice(key)
+func (c *Config) GetInt64Slice(key string) []int64 {
+	ints := c.v.GetIntSlice(key)
 	int64s := make([]int64, 0, len(ints))
 	for _, i := range ints {
 		int64s = append(int64s, int64(i))
@@ -134,15 +196,27 @@ func GetInt64Slice(key string) []int64 {
 
 	return int64s
 }
+func GetInt64Slice(key string) []int64 {
+	return globalConfig.GetInt64Slice(key)
+}
 
+func (c *Config) GetStringSlice(key string) []string {
+	return c.v.GetStringSlice(key)
+}
 func GetStringSlice(key string) []string {
-	return _Viper.GetStringSlice(key)
+	return globalConfig.GetStringSlice(key)
 }
 
+func (c *Config) GetDuration(key string) time.Duration {
+	return c.v.GetDuration(key)
+}
 func GetDuration(key string) time.Duration {
-	return _Viper.GetDuration(key)
+	return globalConfig.GetDuration(key)
 }
 
+func (c *Config) GetTime(key string) time.Time {
+	return c.v.GetTime(key)
+}
 func GetTime(key string) time.Time {
-	return _Viper.GetTime(key)
+	return globalConfig.GetTime(key)
 }
