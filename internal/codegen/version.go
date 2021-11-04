e5bc296f1457afb35c782b45c5234001c6c1178c
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2021-11-04 13:33:36 +0300

    fix skel file

--- internal/codegen/version.go
+++ internal/codegen/version.go
@@ -3,6 +3,6 @@ package codegen
 // This is auto-generated code; DO NOT EDIT.
 
 var (
-	Commit  = "bfc6a025bd231c1fc97e0e6ecd1b9dda0b224aef"
-	Version = "v0.14.36"
+	Commit  = "b37d5c544a57aaa5538ddb918e8d48dceee5fbbe"
+	Version = "v0.15.0"
 )
