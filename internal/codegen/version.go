2809241736b8fa2717264a78f0d29387dc083e3c
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2021-11-17 11:56:39 +0300

    refactor log package to support injecting writer into the core

--- internal/codegen/version.go
+++ internal/codegen/version.go
@@ -3,6 +3,6 @@ package codegen
 // This is auto-generated code; DO NOT EDIT.
 
 var (
-	Commit  = "e1d0ba506752d599a9a7a4dcad482f668bbb8b45"
+	Commit  = "ff883824bf57bf020d649c3ad83dbd1c95593ddb"
 	Version = "v0.15.1"
 )
