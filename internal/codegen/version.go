c8fdb5a6cfc8fc92f1693aed188936a934cebe37
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-01-12 14:42:29 +0400

    improve http status

--- internal/codegen/version.go
+++ internal/codegen/version.go
@@ -3,6 +3,6 @@ package codegen
 // This is auto-generated code; DO NOT EDIT.
 
 var (
-	Commit  = "a34bc350dd22c593b72ecf9ad70e45c5e3d9db7d"
+	Commit  = "e5a02a53fb1a9ab591c16d5e6882c951193a5fbd"
 	Version = "v0.16.22"
 )
